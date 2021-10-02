bf632a428bbe3811f7310a1b11611c9a0fdc8ffd
Qria <qria@qria.net>
2021-10-02 17:38:01 +0900

    Add cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import TinderCard from 'react-tinder-card';
+
+
+const db = [
+  {
+    name: 'Richard Hendricks'
+  },
+  {
+    name: 'Erlich Bachman'
+  },
+  {
+    name: 'Monica Hall'
+  },
+  {
+    name: 'Jared Dunn'
+  },
+  {
+    name: 'Dinesh Chugtai'
+  }
+]
 
 function App() {
+  
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      
+      <h1>Tiiinder</h1>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
     </div>
   );
 }
